c733df38e2134e40aa8b493572c56762e4e54c0c
phuonglh <phuonglh@gmail.com>
2021-04-14 22:35:12 +0700

    Update nlu module

--- VLP/src/nlu/GRU3.jl
+++ VLP/src/nlu/GRU3.jl
@@ -13,7 +13,8 @@ GRU3(inp::Int, out::Int) = GRU3(GRU(inp, out))
 # overload call, so the object can be used as a function
 (f::GRU3)(x::Array{Float32,3}) = begin
     m = size(x)[3]
-    xs = map(i -> f.gru(x[:,:,i])[:,end], collect(1:m))
+    # xs = map(i -> f.gru(x[:,:,i])[:,end], collect(1:m)) # take the last output state
+    xs = map(i -> sum(f.gru(x[:,:,i]), dims=2), collect(1:m)) # take the bag-of-embeddings
     hcat(xs...)
 end
 
