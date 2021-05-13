96d0e99363eb454b69e45b0fb760a59b10feb9f5
phuonglh <phuonglh@gmail.com>
2021-05-13 20:38:39 +0700

    Update nmt module

--- VLP/src/nmt/Corpus.jl
+++ VLP/src/nmt/Corpus.jl
@@ -20,3 +20,33 @@ function readCorpusEuroparl(options)::Array{Tuple{String,String}}
     targetLines = readlines(options[:targetCorpusPath])
     collect(zip(sourceLines, targetLines))
 end
+
+"""
+    saveIndex(index, path)
+    
+    Save an index to an external file.
+"""
+function saveIndex(index, path)
+    file = open(path, "w")
+    for f in keys(index)
+        write(file, string(f, " ", index[f]), "\n")
+    end
+    close(file)
+end
+
+"""
+    loadIndex(path)
+
+    Load an index from a file which is previously saved by `saveIndex()` function.
+"""
+function loadIndex(path)::Dict{String,Int}
+    lines = readlines(path)
+    pairs = Array{Tuple{String,Int},1}()
+    for line in lines
+        j = findlast(' ', line)
+        i = parse(Int, line[j+1:end])
+        w = line[1:prevind(line,j)]
+        push!(pairs, (w, i))
+    end
+    return Dict(pair[1] => pair[2] for pair in pairs)
+end
--- VLP/src/nmt/SS.jl
+++ VLP/src/nmt/SS.jl
@@ -7,6 +7,9 @@ using MicroCollections
 using StatsBase
 
 
+include("../tok/VietnameseTokenizer.jl")
+using .VietnameseTokenizer
+
 include("Embedding.jl")
 include("Options.jl")
 include("Corpus.jl")
@@ -31,6 +34,7 @@ function vocab(sentences::Array{String}, options)::Dict{String,Int}
     wordFrequency = Dict{String, Int}()
     for token in tokens
         word = lowercase(strip(token))
+        if VietnameseTokenizer.shape(word) == "number" word = "[NUM]"; end
         haskey(wordFrequency, word) ? wordFrequency[word] += 1 : wordFrequency[word] = 1
     end
     # filter out infrequent words
@@ -40,14 +44,16 @@ function vocab(sentences::Array{String}, options)::Dict{String,Int}
 end
 
 function batch(pairs::Array{Tuple{String,String}}, sourceDict, targetDict, options)
+    @infor "Create batches for training. Please wait..."
     Xs, Ys = Array{Array{Int},1}(), Array{Array{Int},1}()
     b = options[:batchSize]
     m = options[:maxSequenceLength]
+    transform(word) = if VietnameseTokenizer.shape(word) == "number" word = "[NUM]"; else word end
     for pair in pairs
         srcWords = lowercase.(string.(split(pair[1], options[:delimiters])))
         tarWords = lowercase.(string.(split(pair[2], options[:delimiters])))
-        X = map(word -> get(sourceDict, word, sourceDict[options[:UNK]]), srcWords)
-        Y = map(word -> get(targetDict, word, targetDict[options[:UNK]]), tarWords)
+        X = map(word -> get(sourceDict, transform(word), sourceDict[options[:UNK]]), srcWords)
+        Y = map(word -> get(targetDict, transform(word), targetDict[options[:UNK]]), tarWords)
         if (length(X) < m && length(Y) < m) 
             append!(X, sourceDict[options[:EOS]])
             append!(Y, targetDict[options[:EOS]])
@@ -87,8 +93,8 @@ function train(options)
     pairs = readCorpusEuroparl(options)
     sourceSentences = map(pair -> pair[1], pairs)
     targetSentences = map(pair -> pair[2], pairs)
-    sourceDict = vocab(sourceSentences, options)
-    targetDict = vocab(targetSentences, options)
+    sourceDict = vocab(sourceSentences, options); saveIndex(sourceDict, options[:sourceDictPath])
+    targetDict = vocab(targetSentences, options); saveIndex(targetDict, options[:targetDictPath])
     m, n = length(sourceDict), length(targetDict)
     sourceDict[options[:UNK]] = m+1; sourceDict[options[:EOS]] = m+2; sourceDict[options[:PAD]] = m+3
     targetDict[options[:UNK]] = n+1; targetDict[options[:EOS]] = n+2; targetDict[options[:PAD]] = n+3
@@ -176,11 +182,9 @@ function evaluate(machine, Xbs, Ybs, paddingY, maxSeqLen)
         # number of tokens and number of matches in this batch
         tokens, matches = 0, 0
         for t=1:length(Yb)
-            local k = options[:maxSequenceLength]
+            local k = maxSeqLen
             # find the last position of non-padded element
-            while Yb[t][k] == paddingY
-                k = k - 1
-            end
+            while (Yb[t][k] == paddingY) k = k - 1; end
             tokens += k
             matches += sum(Ŷb[t][1:k] .== Yb[t][1:k])
         end
