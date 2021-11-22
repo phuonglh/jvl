1f98f93fb19e319d26d0a1ee3c10705a2c7b782a
phuonglh <phuonglh@gmail.com>
2021-11-22 22:02:46 +0700

    Update tcp

---
+++ VLP/src/tcp/BinaryNode.jl
@@ -0,0 +1,31 @@
+
+using AbstractTrees
+
+mutable struct BinaryNode{T}
+  data::T
+  parent::BinaryNode{T}
+  left::BinaryNode{T}
+  right::BinaryNode{T}
+
+  # root constructor
+  BinaryNode{T}(data) where T = new{T}(data)
+  # child node constructor
+  BinaryNode{T}(data, parent) where T = new{T}(data, parent)
+end
+
+BinaryNode(data) = BinaryNode{typeof(data)}(data)
+
+"Add left child with some data to a parent."
+function addLeftChild(data, parent::BinaryNode)
+  !isdefined(parent, :left) || error("Left child is already assigned!")
+  node = typeof(parent)(data, parent)
+  parent.left = node
+end
+
+"Add right child with some data to a parent."
+function addRightChild(data, parent::BinaryNode)
+  !isdefined(parent, :right) || error("Right child is already assigned!")
+  node = typeof(parent)(data, parent)
+  parent.right = node
+end
+
